e723c012d1c9a2c3dd19a1b4a6d9f8f0ab8192b1
Faiz Maricar <maricar.faiz@gmail.com>
2020-11-27 16:48:39 +0800

    Edit site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Faiz Maricar`,
+    description: `I'm a front-end developer`,
     author: `@gatsbyjs`,
   },
   plugins: [
