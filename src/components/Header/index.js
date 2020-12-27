3e69d039c08ab9d1e616af726a2dcd14a312d6d6
Faiz Maricar <maricar.faiz@gmail.com>
2020-12-27 20:57:06 +0800

    use menu component in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,15 @@
 import React from "react"
 
-import { scrollToSection } from "../../utils/scroll"
-
+import { Menu } from ".."
 import "./index.css"
 
 const Header = () => {
   return (
     <header>
       <h3>Faiz Maricar</h3>
-      <nav>
-        <ul>
-          <li onClick={scrollToSection}>About</li>
-          <li onClick={scrollToSection}>Portfolio</li>
-          <li onClick={scrollToSection}>Contact</li>
-        </ul>
-      </nav>
+      <div style={{ marginRight: "128px" }}>
+        <Menu />
+      </div>
     </header>
   )
 }
