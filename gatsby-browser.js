792b4ba69fe5b079309a7da5d2a73ce2571afc46
Faiz Maricar <maricar.faiz@gmail.com>
2021-07-04 21:13:17 +0800

    added favicon and title

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,15 @@
 import React from "react"
 import { GlobalStyles } from "@components"
+import { Helmet } from "react-helmet"
 
-export const wrapPageElement = ({ element, props }) => {
+export const wrapPageElement = ({ element }) => {
   return (
     <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Faiz Maricar - Full-stack developer</title>
+        <link rel="canonical" href="https://faizmaricar.github.io" />
+      </Helmet>
       <GlobalStyles />
       {element}
     </React.Fragment>
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,17 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: { name: `images`, path: `${__dirname}/src/images` },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/images/myicon.png`,
+      },
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
