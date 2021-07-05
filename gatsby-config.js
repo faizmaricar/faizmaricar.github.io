eddd7e502d046e4b6a8bf1fe96bd63c116cfe30d
Faiz Maricar <maricar.faiz@gmail.com>
2021-07-05 20:12:09 +0800

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,9 @@ module.exports = {
   plugins: [
     `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: { name: `images`, path: `${__dirname}/src/images` },
